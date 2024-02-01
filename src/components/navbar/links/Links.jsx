4fb9f1fcc6e420e63dfa6009ecc11c1419e4951c
chadley antonels <chadleycantonels@gmail.com>
2024-02-01 13:31:43 +0200

    update

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Links = () => {
+    [
+        {
+            title : "Homepage",
+            path : "/",
+        },
+        {
+            title : "About",
+            path : "/about",
+        },
+        {
+            title : "Contact",
+            path : "/contact",
+        },
+        {
+            title : "Blog",
+            path : "/blog",
+        },
+    ]
+
+  return (
+    <div>
+        {Links.map((link=>(
+            <Link href={link.path} key={link.title}>{link.title}</Link>
+        )))}
+    </div>
+  )
+}
+
+export default Links
