2af215c4572178516c3797e41c1b911c83628a9c
chadley antonels <chadleycantonels@gmail.com>
2024-02-03 12:53:40 +0200

    commit

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
+import styles from './links.module.css'
+
+import NavLink from './navLink'
 
 const Links = () => {
     const links =
@@ -23,9 +26,9 @@ const Links = () => {
     ]
 
   return (
-    <div>
+    <div className={styles.links}>
         {links.map((link=>(
-            <Link href={link.path} key={link.title}>{link.title}</Link>
+            <NavLink item={link} key={link.title}></NavLink>
         )))}
     </div>
   )
