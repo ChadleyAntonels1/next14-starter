1fe215949ea3bea562d86ef2f21a000c8d7418c7
chadley antonels <chadleycantonels@gmail.com>
2024-05-09 15:22:23 +0200

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,7 +8,7 @@
        console.log("\x1b[42m","mongo is connected");
        return;
      }
-     const db = await mongoose.connect(process.env.MONGO,!process.env.MONGODB_URI,);
+     const db = await mongoose.connect(process.env.MONGO,process.env.MONGODB_URI,);
      connection.isConnected = db.connections[0].readyState;
    } catch (error) {
      console.log("\x1b[41m", '%c mongo not connected');
@@ -16,3 +16,27 @@
    }
  };
 
+// import { MongoClient } from "mongodb";
+
+// const uri = process.env.MONGODB_URI
+// const mongo = process.env.MONGO
+// const options ={}
+
+// let mongoClient
+
+// if(!process.env.MONGODB_URI){
+//   throw new Error("please add your Mongo URI to .env.local")
+// }
+
+// export async function connectToDb(){
+//   try {
+//     if(MongoClient){
+//       return{MongoClient}
+//     }
+//     mongoClient= await(new MongoClient(uri,mongo,options)).connect()
+//     console.log("Mongo Connected!")
+//     return{MongoClient}
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
