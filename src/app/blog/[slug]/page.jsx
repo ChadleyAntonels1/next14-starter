a7159a79e5186b90545cb0950ab6019da0859d2d
chadley antonels <chadleycantonels@gmail.com>
2024-05-10 19:12:25 +0200

    commit

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -6,8 +6,7 @@ import { getPost } from "@/lib/data";
 
 // FETCH DATA WITH AN API
 const getData = async (slug) => {
-  const res = await fetch(`http://next14-starter-git-main-chadleyantonels1s-projects.vercel.app/api/blog/${slug}`);
-
+  const res = await fetch(`https://next14-starter-mg60qk3pr-chadleyantonels1s-projects.vercel.app/api/blog/${slug}`);
   if (!res.ok) {
     throw new Error("Something went wrong");
   }
