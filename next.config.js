a6b5c8a1abf2f9c2cf907f9dc336dada40fbfdaf
chadley antonels <chadleycantonels@gmail.com>
2024-05-13 16:34:42 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    
-    images:{
-        remotePatterns:[
-            // to add external images
-            {
-                protocol: 'https',
-                hostname: '',
-            }
-        ]
-    }
+    
 }
 
 module.exports = nextConfig
