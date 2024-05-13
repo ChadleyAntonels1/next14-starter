91127d95f3f6b805da709d0a45436d3861e3ef79
chadley antonels <chadleycantonels@gmail.com>
2024-05-13 16:11:09 +0200

    commit

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,37 +1,37 @@
-// import React from 'react'
-// import PostCard from "@/components/postCard/postCard"
-// import styles from './blog.module.css'
+import React from 'react'
+import PostCard from "@/components/postCard/postCard"
+import styles from './blog.module.css'
 // import { getPosts } from "@/lib/data";
 
 
 // FETCH DATA WITH AN API
-// const getData = async () => {
-//   const res = await fetch("http://localhost:3000/api/blog", {next:{revalidate:3600}});
+const getData = async () => {
+  const res = await fetch("https://next14-starter-two.vercel.app/api/blog", {next:{revalidate:3600}});
 
-//   if (!res.ok) {
-//     throw new Error("Something went wrong")
-//   }
+  if (!res.ok) {
+    throw new Error("Something went wrong")
+  }
 
-//   return res.json();
-// };
+  return res.json();
+};
 
-// const BlogPage = async () => {
+const BlogPage = async () => {
 
-//   // FETCH DATA WITH AN API
-//   const posts = await getData();
+  // FETCH DATA WITH AN API
+  const posts = await getData();
 
-//   FETCH DATA WITHOUT AN API
-//   const posts = await getPosts();
+  // FETCH DATA WITHOUT AN API
+  // const posts = await getPosts();
 
-//   return (
-//     <div className={styles.container}>
-//       {posts.map((post) => (
-//         <div className={styles.post} key={post.slug}>
-//           <PostCard post={post} />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
+  return (
+    <div className={styles.container}>
+      {posts.map((post) => (
+        <div className={styles.post} key={post.slug}>
+          <PostCard post={post} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-// export default BlogPage
+export default BlogPage
