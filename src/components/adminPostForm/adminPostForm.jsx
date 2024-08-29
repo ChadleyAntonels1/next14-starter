ab535103c8f139a362bf63e8b349ec33cc7ec5ba
chadley antonels <chadleycantonels@gmail.com>
2024-08-29 15:01:55 +0200

    convert image to Base64

--- src/components/adminPostForm/adminPostForm.jsx
+++ src/components/adminPostForm/adminPostForm.jsx
@@ -3,21 +3,58 @@
 import { addPost } from "@/lib/action";
 import styles from "./adminPostForm.module.css";
 import { useFormState } from "react-dom";
+import { useState } from "react";
+import Image from "next/image";
 
 const AdminPostForm = ({userId}) => {
   const [state, formAction] = useFormState(addPost, undefined);
+
+  const [baseImage, setbaseImage] = useState("")
+
+  const uploadImg = async (e) => {
+    const file = e.target.files[0];
+    const base64 = await convertImageToBase64(file)
+    setbaseImage(base64)
+  }
+
+  const convertImageToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      if (file.type.startsWith('image/')) {
+        const reader = new FileReader();
   
+        reader.onload = function () {
+          const base64String = reader.result?.toString().split(',')[1] || '';
+          resolve('data:image/png;base64,' + base64String);
+        };
+  
+        reader.readAsDataURL(file);
+      } else {
+        reject(new Error('Selected file is not an image.'));
+      }
+    });
+  }
+
   return ( 
-    <form action={formAction} className={styles.container}>
+    <div>           
+
+      <form action={formAction} className={styles.container}>
       <h1>Add New Post</h1>
-      <input type="hidden" name="userId" value={userId} />
-      <input type="text" name="title" placeholder="Title" />
-      <input type="text" name="slug" placeholder="slug" />
-      <textarea type="text" name="desc" placeholder="desc" rows={10} />
-      <button>Add</button>
-      {state?.error}
-    </form>
+
+        <input type="hidden" name="userId" value={userId} />
+        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="slug" placeholder="slug" />
+        <input type="file" onChange={(e)=>{uploadImg(e)}} />
+        <input type="hidden" name="url" value={baseImage} />
+        {/* <Image src={baseImage} width={50} height={50} alt=""/> */}
+        <textarea type="text" name="desc" placeholder="desc" rows={10} />
+        <button>Add</button>
+        {state?.error}
+      </form>
+    </div>
   );
 };
 
 export default AdminPostForm;
+
+
+
